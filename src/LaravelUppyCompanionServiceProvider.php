000d3f9d172a79682933e2c7604cad67e89d8ae6
DP <dp@reproconnect.com>
2022-10-25 10:26:23 -0400

    remove empty boot()

--- src/LaravelUppyCompanionServiceProvider.php
+++ src/LaravelUppyCompanionServiceProvider.php
@@ -6,8 +6,4 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelUppyCompanionServiceProvider extends ServiceProvider
 {
-    public function boot()
-    {
-        // dd(\STS\LaravelUppyCompanion\Http\Controllers\UploadSigningController::class);
-    }
 }
