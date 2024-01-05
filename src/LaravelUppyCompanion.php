eec043d9e859f8aec98070736676f7abdba0ef30
David Palmer <dp88@users.noreply.github.com>
2024-01-05 16:34:48 -0500

    de-static-ify the companion

--- src/LaravelUppyCompanion.php
+++ src/LaravelUppyCompanion.php
@@ -6,53 +6,53 @@ use Aws\S3\S3ClientInterface;
 
 class LaravelUppyCompanion
 {
-    private static \Closure $clientCallback;
-    private static S3ClientInterface $client;
+    private \Closure $clientCallback;
+    private S3ClientInterface $client;
 
-    private static \Closure $bucketCallback;
-    private static string $bucket;
+    private \Closure $bucketCallback;
+    private string $bucket;
 
-    public static function client(\Closure|S3ClientInterface $client)
-    {
-        if ($client instanceof \Closure) {
-            static::$clientCallback = $client;
-        } elseif ($client instanceof S3ClientInterface) {
-            static::$client = $client;
-        }
-    }
+    public function __construct()
+    {}
 
-    public static function bucket(\Closure|string $bucket)
+    public function configure(\Closure|string $bucket, \Closure|S3ClientInterface $client)
     {
         if ($bucket instanceof \Closure) {
-            static::$bucketCallback = $bucket;
-        } elseif (is_string($bucket)) {
-            static::$bucket = $bucket;
+            $this->bucketCallback = $bucket;
+        } else {
+            $this->bucket = $bucket;
+        }
+
+        if ($client instanceof \Closure) {
+            $this->clientCallback = $client;
+        } else {
+            $this->client = $client;
         }
     }
 
-    public static function getClient(): S3ClientInterface
+    public function getClient(): S3ClientInterface
     {
-        if (empty(static::$client)) {
-            if (empty(static::$clientCallback)) {
+        if (empty($this->client)) {
+            if (empty($this->clientCallback)) {
                 throw new \Exception('No client or client callback defined');
             }
 
-            static::$client = call_user_func(static::$clientCallback);
+            $this->client = call_user_func($this->clientCallback);
         }
 
-        return static::$client;
+        return $this->client;
     }
 
-    public static function getBucket(): string
+    public function getBucket(): string
     {
-        if (empty(static::$bucket)) {
-            if (empty(static::$bucketCallback)) {
+        if (empty($this->bucket)) {
+            if (empty($this->bucketCallback)) {
                 throw new \Exception('No bucket or bucket callback defined');
             }
 
-            static::$bucket = call_user_func(static::$bucketCallback);
+            $this->bucket = call_user_func($this->bucketCallback);
         }
 
-        return static::$bucket;
+        return $this->bucket;
     }
 }
